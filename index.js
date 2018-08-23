3ad5d6e95bf6bca23970b8e0e5eb37b5742c161e
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-23 16:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ function lowerCaseDrivers(array){
   return array.map(function(element){
     return element.toLowerCase();
   });
-}
+};
 function nameToAttributes(array){
   return array.map(function(element){
     return {firstName: element.split(' ')[0], lastName: element.split(' ')[1]};
-  })
-}
+  });
+};
+function attributesToPhrase(array){
+  return array.map(function(elment){
+    return `${elment[name]} is from ${elment[hometown]}`;
+  });
+};
